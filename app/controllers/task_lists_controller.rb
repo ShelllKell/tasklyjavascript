a5a56d4c987fdb76c4eee8eee859c704fd4bc376
Shelby Kelly <shelby.kelly@colorado.edu>
2014-08-05 17:03:21 -0600

    first four stories done

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -9,9 +9,34 @@ class TaskListsController < ApplicationController
   end
 
   def create
-    @new_task = TaskList.create(:name => params[:task_list][:name])
+    @task_list = TaskList.new(:name => params[:task_list][:name])
 
-    flash[:notice] = "Task List was created successfully!"
-    redirect_to "/"
+    if  @task_list.save
+      flash[:notice] = "Task list was created successfully!"
+      redirect_to "/"
+    else
+      render :new
+    end
   end
+
+  def edit
+    @task_list = TaskList.find(params[:id])
+  end
+
+  def update
+    @task_list = TaskList.find(params[:id])
+    if @task_list.update(allowed_parameters)
+      flash[:notice] = "Task List was updated successfully!"
+      redirect_to "/"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def allowed_parameters
+    params.require(:task_list).permit(:name)
+  end
+
 end
--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,3 +1,5 @@
 class TaskList < ActiveRecord::Base
 
+  validates :name, presence:{:message => "Your task could not be created"}
+
 end
