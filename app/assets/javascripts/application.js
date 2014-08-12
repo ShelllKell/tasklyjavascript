dc045a2a82bca70a3273cef4566c6c808dfcc666
Shelby Kelly <shelby.kelly@colorado.edu>
2014-08-12 16:15:55 -0600

    trying javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,39 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+
+$(".button").click(function() {
+  $('.tasks').addClass('open');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
